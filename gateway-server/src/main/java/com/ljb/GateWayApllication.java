707c8b1c86587d8e950605520eda63a20ea77e39
liujinbo <1072435568@qq.com>
2019-08-05 08:52:41 +0800

    Initial commit

---
+++ gateway-server/src/main/java/com/ljb/GateWayApllication.java
@@ -0,0 +1,17 @@
+package com.ljb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 刘进波
+ * @create 2019-08-04 20:04
+ */
+@SpringBootApplication
+public class GateWayApllication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GateWayApllication.class,args);
+    }
+
+}
---
+++ gateway-server/src/main/java/com/ljb/common/IdEntity.java
@@ -0,0 +1,14 @@
+package com.ljb.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class IdEntity implements Serializable {
+
+    private Long id;
+
+    private int version;
+
+}
